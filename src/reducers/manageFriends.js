757af83a6434a0c4bacadc078db7b12edea8744c
Andrew Johnson <ajohnson.uva@gmail.com>
2019-04-22 09:37:12 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return { friends: [ ...state.friends, action.friend ]}
+      break;
+    case 'REMOVE_FRIEND':
+      console.log(action)
+      return { friends: state.friends.filter(friend => friend.id !== action.id )}
+      break;
+    default:
+      return state;
+  }
 }
